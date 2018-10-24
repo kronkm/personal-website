34aa452a8ba4f16011be40d6ce1bd78fcf333547
kronkm <mkronk7@gmail.com>
2018-10-23 22:10:38 -0500

    updated content, some style changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,28 +6,43 @@ import '../styles.css'
 const IndexPage = () => (
   <Layout>
     <div className="content-container">
+      <div className="translucent-overlay"></div>
       <div style={{position: 'relative'}}>
-        <h1 style={{
-        }}>Mike Kronk</h1>
+        {/*<div style={{textAlign: 'center'}}>
+          <h1 style={{
+          }}>Mike Kronk</h1>
+          <p>🎮😎👨🏼‍💻🐾🍻</p>
+          <h2>Front-End Developer</h2>
+        </div>*/}
+        <div style={{textAlign: 'center'}}>
+          <h1>Hey! I'm Mike Kronk.</h1>
+          <h2>I'm a Front-End Engineer in Austin, TX.</h2>
+          <p>🎮😎👨🏼‍💻🐾🍻</p>
+        </div>
+        <section className='content-section'>
+          <p>
+            Working on the Front-End is where it's at! I truly enjoy working at the intersection of people and technology that building user-friendly, data-rich interfaces requires.
+          </p>
+        </section>
+
+        <section className='content-section'>
+          <p>
+            My technical skillset has a solid foundation behind it - HTML5, CSS3, and ES6+. Beyond this, my current favorite tools for building user interfaces are React, Sass, and Webpack.
+          </p>
+        </section>
+
+        <section className='content-section'>
+          <p>
+            Besides coding, I believe that soft skills matter just as much as technical ability. Software development is a team sport, after all. To this end, I excel in organizing information from multiple sources and communicating with both technical and non-technical audiences.
+          </p>
+        </section>
+
+        <section style={{textAlign: 'center'}}>
+          <p>
+            Feel free to <a class="contact-me__link" style={{color: '#fff'}} href="mailto:email@mrkronk.com">say hi!</a>
+          </p>
+        </section>
       </div>
-      <p>🎮😎👨🏼‍💻🐾🍻</p>
-      <h2>Front-End Developer</h2>
-
-
-      <section className='content-section'>
-        <p>Working Philosophy:</p>
-        <p>Software development is a team sport.</p>
-      </section>
-
-      <section className='content-section'>
-        <p>Technical Specialties:</p>
-        <p>React - Front-End Architecture - Sass</p>
-      </section>
-
-      <section className='content-section'>
-        <p>Non-Technical Skills:</p>
-        <p>Workflows - Communication - Composure</p>
-      </section>
     </div>
   </Layout>
 )
