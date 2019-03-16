4705c523a54e91482b6559a4769ee8e938b23a2f
kronkm <mkronk7@gmail.com>
2019-03-16 18:25:44 -0500

    fix header footer imports in layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby';
-import Header from './Header';
-import Footer from './Footer';
+import Header from './header';
+import Footer from './footer';
 
 import './layout.css';
 import '../styles.css';
